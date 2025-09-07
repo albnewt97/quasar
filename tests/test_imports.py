119998498e4d4809588cbd541d6a7dad53919fc5
albnewt97 <naman.edu97@gmail.com>
2025-09-07 06:05:18 +0530

    Create test_imports.py

---
+++ tests/test_imports.py
@@ -0,0 +1,6 @@
+import importlib
+
+def test_imports():
+assert importlib.import_module("sequence_ext")
+assert importlib.import_module("sequence_ext.scenarios.scenario1_static")
+assert importlib.import_module("sequence_ext.orchestrator.mdi_controller")
